b0184081dc45d325bd73cfa850e233826bfb28a9
Gabzuno <kgperez120@gmail.com>
2025-05-28 00:29:43 +0800

    added again

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ console.log("Hello World")
 
 console.log("This is a demo script running in the main.js file.");
 console.log("You can modify this file to change the output.");
+console.log("Make sure to run this script in a JavaScript environment to see the output.");
